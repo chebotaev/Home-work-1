b56ae4dbb72dd226dcc988e002e5c51a4055cf8e
chebotaev <chebotaevss@gmail.com>
2019-04-05 10:13:31 +0300

    first commit

---
+++ HW1.js
@@ -0,0 +1,42 @@
+var a = 10;
+var b = 2;
+alert(a + b);
+alert(a - b);
+alert(a * b);
+alert(a / b);
+ 
+var a = 17;
+var b = 10;
+var c = a - b;
+var d = 7;
+var result = c + d;
+alert(result);
+
+var sec = 60;
+var min = 60;
+var hour = 24;
+var day = 30;
+var secInHour = sec * min;
+var secInDay = secInHour * hour;
+var secInMonth = secInDay * day;
+alert(secInHour);
+alert(secInDay);
+alert(secInMonth);
+
+var hour = "21";
+var min = "30";
+var sec = "15";
+alert( hour + ":" + min + ":" + sec);
+
+var a = 2;
+var b = a * a;
+alert(b);
+
+var num = 1;
+num += 12;
+num -= 14;
+num *= 5;
+num /= 7;
+num++;
+num--;
+alert(num);
